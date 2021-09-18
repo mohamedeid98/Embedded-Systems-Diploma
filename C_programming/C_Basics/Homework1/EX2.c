1abb16c3d8f1498410f9afb6189f7e7a9747a7e0
mohamedeid98 <mohamedeid@gmail.com>
2021-09-18 05:31:15 +0200

    EX2

---
+++ C_programming/C_Basics/Homework1/EX2.c
@@ -0,0 +1,20 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Sep 18, 2021
+ *      Author: mohamed
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	printf("Enter a integer: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d", &num);
+	printf("You entered: %d", num);
+
+	return 0;
+}
