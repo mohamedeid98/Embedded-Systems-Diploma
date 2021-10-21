62584b2b69a9a121166267514add6a96824dc9db
mohamedeid98 <mohamedeid@gmail.com>
2021-10-21 18:42:06 +0200

    Ex1

---
+++ C_programming/Structures_Union_Enum/Homework5/EX1.c
@@ -0,0 +1,43 @@
+/*
+ * EX1.c
+ *
+ *  Created on: Oct 21, 2021
+ *      Author: mohamed
+ */
+struct SStudent
+{
+	char name[10];
+	int roll;
+	float marks;
+};
+
+#include <stdio.h>
+
+int main()
+{
+	struct SStudent student;
+
+	printf("Enter information of the student\n");
+
+	printf("Enter name: ");
+	fflush(stdout);
+	scanf("%s", student.name);
+
+	printf("Enter roll number: ");
+	fflush(stdout);
+	scanf("%d", &student.roll);
+
+	printf("Enter marks: ");
+	fflush(stdout);
+	scanf("%f", &student.marks);
+
+	printf("\nDisplay Information\n");
+
+	printf("name: %s\n", student.name);
+	printf("roll: %d\n", student.roll);
+	printf("marks: %0.2f\n", student.marks);
+
+
+
+	return 0;
+}
