a9896827f0faeb61b49e3bf8e029bd7364938496
mohamedeid98 <mohamedeid@gmail.com>
2022-02-10 17:40:17 +0200

    another approach

--- C_programming/Pointers/Assignment/print_string_in_reverse_using_pointers.c
+++ C_programming/Pointers/Assignment/print_string_in_reverse_using_pointers.c
@@ -3,7 +3,34 @@
 
 int main()
 {
-	int i;
+	int i=0;
+	char s[20];
+	char *ptr = s;
+	printf("Input a string: ");
+	fflush(stdout);
+	scanf("%s", s);
+
+	while(*ptr)
+	{
+		ptr++;
+		i++;
+	}
+	//printf("string base address = 0x%x\nlast character address = 0x%x\nnumber of characters = %d",s,ptr,i);
+	while(i)
+	{
+		ptr--;
+		printf("%c",*ptr);
+		i--;
+	}
+
+
+
+
+
+
+
+
+/*	int i;
 	char s[20];
 
 	printf("Input a string: ");
@@ -14,7 +41,7 @@ int main()
 	{
 		printf("%c", *ptr);
 		ptr--;
-	}
+	}*/
 
 
 
