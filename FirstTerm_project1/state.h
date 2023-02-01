c3fab8e08c6a792462cb9a8768701a002691084c
Mohamed Eid <m0hamed.3ed98@gmail.com>
2023-02-01 08:22:46 +0200

    initial commit for actual implementation

---
+++ FirstTerm_project1/state.h
@@ -0,0 +1,14 @@
+#ifndef STATE_H_
+#define STATE_H_
+
+#define STATE_define(_stateFUN)     void ST_##_stateFUN()
+#define STATE(_stateFUN)            ST_##_stateFUN
+
+
+
+
+
+
+
+
+#endif /* STATE_H_ */
