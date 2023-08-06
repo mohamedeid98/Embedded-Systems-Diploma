823f2cb06e0c711cfec980e7fffe14067468cb17
Mohamed Eid <m0hamed.3ed98@gmail.com>
2023-08-06 05:56:28 +0300

    UART_ATmega32 first commit

---
+++ MCU Interfacing/UART_ATmega32/UART_ATmega32/UART_ATmega32/main.c
@@ -0,0 +1,18 @@
+/*
+ * UART_ATmega32.c
+ *
+ * Created: 8/6/2023 5:56:10 AM
+ * Author : Mohamed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
